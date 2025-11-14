01654414450d8d565b721c77abe09d6a30dba7f8
gusjjpv <joaogustavolima084@gmail.com>
2025-11-14 18:37:42 -0300

    feat: crud professor

--- PERFIT/academia/views.py
+++ PERFIT/academia/views.py
@@ -1,1 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Professor
+from .serializers import ProfessorSerializer, ProfessorCreateSerializer
+
+class ProfessoresAPIView(generics.ListCreateAPIView):
+    queryset = Professor.objects.all()
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return ProfessorCreateSerializer
+        return ProfessorSerializer
+
+
+class ProfessorAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Professor.objects.all()
+    serializer_class = ProfessorSerializer
