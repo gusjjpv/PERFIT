7c36d89ff44c70514af5cdc563f6ac00eb159fa9
gusjjpv <joaogustavolima084@gmail.com>
2025-11-15 12:25:51 -0300

    feat:rotas login e aluno

--- PERFIT/academia/urls.py
+++ PERFIT/academia/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('profesores/', views.ProfessoresAPIView.as_view(), name='professor'),
-    path('profesores/<int:pk>/', views.ProfessorAPIView.as_view(), name='profesores')
+    path('profesores/<int:pk>/', views.ProfessorAPIView.as_view(), name='profesores'),
+    path('alunos/', views.AlunosAPIView.as_view(), name='alunos-list'),
+    path('alunos/<int:pk>/', views.AlunoAPIView.as_view(), name='aluno-detail')
 ]
