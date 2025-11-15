962dd9a933d4c21f83f33d62f32bb2634cf78f60
gusjjpv <joaogustavolima084@gmail.com>
2025-11-15 12:24:11 -0300

    feat: crud aluno

--- PERFIT/academia/views.py
+++ PERFIT/academia/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
-from .models import Professor
-from .serializers import ProfessorSerializer, ProfessorCreateSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .models import Professor, Aluno
+from .serializers import ProfessorSerializer, ProfessorCreateSerializer, AlunoSerializer, AlunoCreateSerializer
+from .permissions import IsProfessor
 
 class ProfessoresAPIView(generics.ListCreateAPIView):
     queryset = Professor.objects.all()
@@ -9,8 +11,33 @@ class ProfessoresAPIView(generics.ListCreateAPIView):
         if self.request.method == 'POST':
             return ProfessorCreateSerializer
         return ProfessorSerializer
+    
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            self.permission_classes = [AllowAny]
+        else:
+            self.permission_classes = [IsAuthenticated]
+        return super().get_permissions()
 
 
 class ProfessorAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Professor.objects.all()
     serializer_class = ProfessorSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class AlunosAPIView(generics.ListCreateAPIView):
+    queryset = Aluno.objects.all()
+    permission_classes = [IsAuthenticated, IsProfessor]
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return AlunoCreateSerializer
+        return AlunoSerializer
+
+
+class AlunoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+    permission_classes = [IsAuthenticated]
+
