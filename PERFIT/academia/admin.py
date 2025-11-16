87a1bd78936157d12959a20d6b24c30c87fe4e19
Gustavo Fontes <fontes.gustavo519@hotmail.com>
2025-11-16 00:16:37 -0300

    feat: registrar models no Django Admin
    Adiciona configuração do Django Admin para os models:
    - Professor: exibe user e CREF
    - Aluno: exibe user e professor
    - FichaDeDados: exibe dados físicos com IMC readonly
    - AvaliacaoFisica: exibe medidas corporais
    - AvaliacaoPa: exibe pressão arterial com momento
    
    Facilita gerenciamento e testes durante desenvolvimento.

--- PERFIT/academia/admin.py
+++ PERFIT/academia/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Professor, Aluno, FichaDeDados, AvaliacaoFisica, AvaliacaoPa
 
-# Register your models here.
+@admin.register(Professor)
+class ProfessorAdmin(admin.ModelAdmin):
+    list_display = ['user', 'cref']
+    search_fields = ['user__username', 'user__first_name', 'user__last_name', 'cref']
+
+@admin.register(Aluno)
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ['user', 'professor']
+    search_fields = ['user__username', 'user__first_name', 'user__last_name']
+    list_filter = ['professor']
+
+@admin.register(FichaDeDados)
+class FichaDeDadosAdmin(admin.ModelAdmin):
+    list_display = ['aluno', 'altura', 'peso', 'imc']
+    search_fields = ['aluno__user__username', 'aluno__user__first_name']
+    readonly_fields = ['imc']
+
+@admin.register(AvaliacaoFisica)
+class AvaliacaoFisicaAdmin(admin.ModelAdmin):
+    list_display = ['aluno', 'data', 'peito', 'cintura']
+    search_fields = ['aluno__user__username', 'aluno__user__first_name']
+    list_filter = ['data']
+
+@admin.register(AvaliacaoPa)
+class AvaliacaoPaAdmin(admin.ModelAdmin):
+    list_display = ['aluno', 'data', 'momento', 'paSistolica', 'paDiastolica']
+    search_fields = ['aluno__user__username', 'aluno__user__first_name']
+    list_filter = ['data', 'momento']
