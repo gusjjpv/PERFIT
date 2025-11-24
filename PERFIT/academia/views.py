d4758b84c85dfcc7e74f10792ccbd4156d4b1c05
gusjjpv <joaogustavolima084@gmail.com>
2025-11-24 19:00:55 -0300

    returns com msgs

--- PERFIT/academia/views.py
+++ PERFIT/academia/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from .models import Professor, Aluno
 from .serializers import ProfessorSerializer, ProfessorCreateSerializer, AlunoSerializer, AlunoCreateSerializer
@@ -83,6 +84,22 @@ class AlunosAPIView(generics.ListCreateAPIView):
             return AlunoCreateSerializer
         return AlunoSerializer
     
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        
+        return Response(
+            {
+                "message": "Aluno cadastrado com sucesso!",
+                "data": serializer.data
+            },
+            status=status.HTTP_201_CREATED,
+            headers=headers
+        )
+
     #sobrescrevendo os metodos para documentacao no swagger
 
     @extend_schema(
@@ -113,6 +130,34 @@ class AlunoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Aluno.objects.all()
     serializer_class = AlunoSerializer
     permission_classes = [IsAuthenticated]
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        return Response(
+            {
+                "message": "Dados do aluno atualizados com sucesso!",
+                "data": serializer.data
+            },
+            status=status.HTTP_200_OK
+        )
+    
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        
+        instance.ativo = False
+        instance.save()
+        
+        return Response(
+            {
+                "message": "Aluno desativado com sucesso!"
+            },
+            status=status.HTTP_200_OK
+        )
 
     #sobrescrevendo os metodos para documentacao no swagger
 
